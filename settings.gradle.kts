cc828151ec2e5309c3a8f1de92864f783bb92e5a
marko.vukosav <marko.vukosav@rimac-automobili.com>
2022-04-22 10:31:09 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Find_Time_KMM"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/findtimekmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.findtimekmm
+
+expect class Platform() {
+    val platform: String
+}
